8de1aa6e19446a98c7eff1d1885fe0ad1249d592
Max Prettyjohns <heshoots9999@gmail.com>
2019-06-05 17:28:42 +0100

    fix broken ui assets

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,6 @@ RUN rm -r /opt/ldregistry/boot/*
 COPY bin/entrypoint.sh entrypoint.sh
 COPY templates/app.conf.tmpl /etc/templates/app.conf.tmpl
 COPY templates/user.ini.tmpl /etc/templates/user.ini.tmpl
-COPY templates/server.xml.tmpl /etc/templates/server.xml.tmpl
 COPY templates/_navbar.vm.tmpl /etc/templates/_navbar.vm.tmpl
 # Newer version of tomcat complain about cookies containing whitespace
 COPY context.xml /usr/local/tomcat/conf/context.xml
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -12,9 +12,10 @@ if [ -z $USER_PASSWORD ]; then
   export USER_PASSWORD="changeme"
 fi
 echo $USER_LOGIN $USER_PASSWORD
+mv /opt/ldregistry/registry.war /usr/local/tomcat/webapps/$REGISTRY_BASE_PATH.war
 envsubst '${REGISTRY_BASE_URI}' < /etc/templates/app.conf.tmpl > /opt/ldregistry/config/app.conf
 envsubst '${REGISTRY_BASE_PATH}' < /etc/templates/_navbar.vm.tmpl > /opt/ldregistry/templates/nav/_navbar.vm
 envsubst < /etc/templates/user.ini.tmpl > /opt/ldregistry/config/user.ini
-envsubst < /etc/templates/server.xml.tmpl > /usr/local/tomcat/conf/server.xml
+#envsubst < /etc/templates/server.xml.tmpl > /usr/local/tomcat/conf/server.xml
 rm /var/opt/ldregistry/store/tdb.lock
 catalina.sh run
