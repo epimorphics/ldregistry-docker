8af747d8d3abee0ca45e9805ce2786d3062bf482
Max Prettyjohns <heshoots9999@gmail.com>
2019-06-07 14:54:24 +0100

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,14 +7,13 @@ WORKDIR /app
 COPY registry-core/ .
 RUN mvn clean package -DskipTests
 
-FROM tomcat:8-jre8-alpine
+FROM tomcat:7-jre8-alpine
 RUN apk add --no-cache --update gettext
 COPY --from=builder /app/target/registry-core-2.0.1-SNAPSHOT.war /opt/ldregistry/registry.war
 RUN mkdir -p /var/opt/ldregistry/logstore
 COPY registry-config-base/ldregistry /opt/ldregistry
-RUN rm -r /opt/ldregistry/boot/*
 COPY bin/entrypoint.sh entrypoint.sh
 COPY templates/ /etc/templates/
-# Newer version of tomcat complain about cookies containing whitespace
-COPY context.xml /usr/local/tomcat/conf/context.xml
+# Newer versions of tomcat complain about cookies containing whitespace
+#COPY context.xml /usr/local/tomcat/conf/context.xml
 CMD ["./entrypoint.sh"]
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+setProperty () {
+  echo "SETTING PROPERTY: $1 = $2"
+  sed -i "s|\($1[ \t]*=[ \t]*\)\(.*\)|\1 $2|g" /opt/ldregistry/config/app.conf
+}
+
 if [ -z $REGISTRY_BASE_PATH ]; then
   export REGISTRY_BASE_PATH=registry
 fi
-if [ -z $REGISTRY_BASE_URI ]; then
-  REGISTRY_BASE_URI="http://localhost/registry"
+if [ $REGISTRY_BASE_URI ]; then
+  # replace registry.baseUri with value of $REGISTRY_BASE_URI
+  setProperty "registry.baseUri" $REGISTRY_BASE_URI
 fi
 if [ -z $USER_LOGIN ]; then
   export USER_LOGIN="dave@epimorphics.com"
@@ -11,14 +18,18 @@ fi
 if [ -z $USER_PASSWORD ]; then
   export USER_PASSWORD="changeme"
 fi
+if [ $REGISTRY_REDIRECT_HTTPS_LOGIN ]; then
+  # replace registry.baseUri with value of $REGISTRY_BASE_URI
+  setProperty "registry.redirectToHttpsOnLogin" $REGISTRY_REDIRECT_HTTPS_LOGIN
+fi
+
 # set war to be at 8080/$REGISTRY_BASE_PATH
 mv /opt/ldregistry/registry.war /usr/local/tomcat/webapps/$REGISTRY_BASE_PATH.war
-
-# replace registry.baseUri with value of $REGISTRY_BASE_URI
-sed -i "s|\(registry.baseUri[ \t]*=[ \t]*\)\(.*\)|\1 $REGISTRY_BASE_URI|g" /opt/ldregistry/config/app.conf
 
 # setup user with admin and password
 envsubst < /etc/templates/user.ini.tmpl > /opt/ldregistry/config/user.ini
 
-rm /var/opt/ldregistry/store/tdb.lock
+rm -f /var/opt/ldregistry/store/tdb.lock
+rm -f /var/opt/ldregistry/index/write.lock
+
 catalina.sh run
